46e55bf2c5edc6e8d4f074347973e57024447b09
Toriku <anatoly.kravtsov@gmail.com>
2021-02-19 13:19:42 +0500

    feat: brackets done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let a = bracketsConfig.map((e) => e.join(""));
+
+    for (let index = 0; index < a.length; index++) {
+        if (str.includes(a[index])) {
+            str = str.replace(a[index], "");
+            index = -1;
+        }
+    }
+
+    if (str.length == 0) {
+        return true;
+    } else return false;
 }
